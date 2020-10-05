39c07d49c7210e5ab4ea1170a9e87e68fd0d49ad
Ryan Camp <campryan@comcast.net>
2020-10-05 16:30:42 -0600

    Initial Commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="This is Ryan's porfolio site" />
   </div>
 </template>
 
