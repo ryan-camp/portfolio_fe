c51fad2a86ee71dcd4de3c98c7acfad646fb998d
Ryan Camp <campryan@comcast.net>
2020-10-05 18:08:53 -0600

    Get rid of needless config file and change title

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 
-# abort on errors
 set -e
 
 npm run build
@@ -11,8 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/campprg/portfolio.git master:gh-pages
+git push -f https://github.com/campprg/campprg.github.io.git master:gh-pages
 
 cd -
