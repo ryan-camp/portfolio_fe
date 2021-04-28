9cf0f5aabf26ac32643c91e8eafaab23a163ac53
Ryan Camp <campryan@comcast.net>
2021-04-28 10:13:13 -0600

    Update about page with generic cover letter

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="about">
-    <h1>This will be the about page for Ryan Camp</h1>
+    <p>I love to develop technology that helps people in their everyday lives.
+      I have experience working in Ruby, RoR, Python, Django, Vue, and React.
+      I have also worked with GraphQL and Postgresql.
+      Learning new ideas, philosophies and technologies is what drives me on the day to day.
+      I am available Monday - Friday 9:00AM - 5:00PM MST.
+      Please, reach out, I would love to talk about how I can help you and your business.
+      Have a great day!</p>
   </div>
 </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
